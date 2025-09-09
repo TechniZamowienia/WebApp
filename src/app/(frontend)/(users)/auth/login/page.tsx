78783090e9ef3801b81b3c9e0721ef5f6f6818dd
SmoczaSkala <99537042+SmoczaSkala@users.noreply.github.com>
2025-09-09 13:19:54 +0200

    pushniete node_modules tego typu

--- src/app/(frontend)/(users)/auth/login/page.tsx
+++ src/app/(frontend)/(users)/auth/login/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function LoginPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: 40 }}>
-      <SignIn routing="hash" signUpUrl="/auth/signup" afterSignInUrl="/" />
+      <SignIn routing="hash" signUpUrl="/auth/signup" afterSignInUrl="/orders-view" />
     </div>
   )
 }
